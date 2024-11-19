58d162cdb232c5705948787600b4426ca2785be5
FahimAbrar0112 <fahimabrar20@iut-dhaka.edu>
2024-11-20 03:08:05 +0600

    testing 1

--- Codes/PreComputation/teset.cpp
+++ Codes/PreComputation/teset.cpp
@@ -1,37 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(void)
+int main()
 {
-
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        int n;
-        cin >> n;
-        vector<int> v;
-
-        for (int i = 1; i <= n; i++)
-        {
-            int x;
-            cin >> x;
-            v.push_back(x);
-        }
-
-        sort(v.begin(), v.end());
-
-        int q;
-        cin >> q;
-
-        while (q--)
-        {
-            int x;
-            cin >> x;
-            int i = 0;
-            while (true)
-            {
-            }
-        }
-    }
+    cout << "GG";
 }
