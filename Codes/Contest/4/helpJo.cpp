6e9fbae6fd97ab7309e50d75456a529049c9b137
FahimAbrar0112 <fahimabrar20@iut-dhaka.edu>
2024-12-09 04:46:36 +0600

    helpjo

---
+++ Codes/Contest/4/helpJo.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5 + 7;
+const int INF = INT_MAX;
+
+int main(void)
+{
+    int n, m;
+    cin >> n >> m;
+    vector<long long> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+
+    int q;
+    cin >> q;
+
+    while (q--)
+    {
+        int x;
+        cin >> x;
+
+        long long maxV = -1;
+        for (auto value : v)
+        {
+            maxV = max(((value + x) % m), maxV);
+        }
+        cout<<maxV<<endl;
+    }
+
+    return 0;
+}
