0ffd6e19f46e94dbfbdfe575c515340158a0923f
FahimAbrar0112 <fahimabrar20@iut-dhaka.edu>
2024-12-13 04:53:13 +0600

    1.cpp

--- Trial/1.cpp
+++ Trial/1.cpp
@@ -1,11 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-
 int main(void)
 {
 
      std::cout << "Size of long long int: " << sizeof(long long int) * 8 << " bits\n";
      std::cout << "Size of unsigned long long int: " << sizeof(unsigned long long int) * 8 << " bits\n";
+     cout << "GG";
 }
